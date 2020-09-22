a5c95b406bd7a45a8bfadf18b65c34d27b1f3726
amorking <69198429+amorking@users.noreply.github.com>
2020-09-23 05:38:14 +0900

    vue deploy eidt for CNAME

--- deploy.sh
+++ deploy.sh
@@ -10,7 +10,7 @@ npm run build
 cd dist
 
 # if you are deploying to a custom domain
-echo 'hwoa-rang.github.io' > 'decide.kro.kr'
+echo 'https://hwoa-rang.github.io/Team/' > 'decide.kro.kr'
 
 git init
 git add -A
